0d8f12b0e8b270ee5f226c5f91ae940e32f47895
Yeok-c <yeokch@gmail.com>
2022-09-20 16:02:12 +0800

    Update motor_control_2.py

--- python/motor_control_2.py
+++ python/motor_control_2.py
@@ -37,7 +37,7 @@ class MotorController:
     def str_to_hex(self, hex_string):
         # hex_string = "0xFF"
         an_integer = int(hex_string, 16)
-        hex_value = hex(an_integer)
+        # hex_value = hex(an_integer)
         return an_integer
 
     def get_crc_value(self, s, crc16):
@@ -49,52 +49,39 @@ class MotorController:
         if len(str_list) == 4:
             str_list.insert(2, '00')  # 位数不足补0
         crc_data = ''.join(str_list[2:])
-        # return crc_data[:2] + ' ' + crc_data[2:]
-        # return crc_data[2:] + crc_data[:2]
         return self.str_to_hex("0x" + crc_data[2:]), self.str_to_hex("0x" + crc_data[:2])
 
-
-    def forward_n_step(self, n):
-        addr = 1
-        func_code = 16
-        reg_addr = 2004  # forward
-        num_regs = 2
-        data_len = 4
-        reg_value = n
-
+    def generate_serial_list(self, addr, func_code, reg_addr, num_regs, data_len, reg_value):
+        # data before appending CRC
         data_to_crc = self.int_hex2(addr) + self.int_hex2(func_code) + self.int_hex4(reg_addr) + self.int_hex4(
             num_regs) + self.int_hex2(data_len) + self.int_hex8(reg_value)
+        
+        assert len(data_to_crc)%2==0 # Just checking if problem is here
 
+        # generate CRC
         p1, p2 = self.crc16_modbus(data_to_crc)
 
+        # append CRC behind data
         data_with_crc = data_to_crc + self.int_hex2(p1) + self.int_hex2(p2)
 
+        # append 0x to each item and form into string
         mylist = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
         for i in range(len(mylist)):
             mylist[i] = self.str_to_hex('0x' + data_with_crc[i * 2:(i + 1) * 2])
 
-        self.ser_send(mylist)
+        return mylist
 
-
-    def backward_n_step(self, n):
+    def n_step(self, n):
         addr = 1
         func_code = 16
-        reg_addr = 2006  # backward
+        if n>0:
+            reg_addr = 2004  # forward
+        else:
+            reg_addr = 2006  # backward
         num_regs = 2
         data_len = 4
-        reg_value = n
-
-        data_to_crc = self.int_hex2(addr) + self.int_hex2(func_code) + self.int_hex4(reg_addr) + self.int_hex4(
-            num_regs) + self.int_hex2(data_len) + self.int_hex8(reg_value)
-
-        p1, p2 = self.crc16_modbus(data_to_crc)
-
-        data_with_crc = data_to_crc + self.int_hex2(p1) + self.int_hex2(p2)
-
-        mylist = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-        for i in range(len(mylist)):
-            mylist[i] = self.str_to_hex('0x' + data_with_crc[i * 2:(i + 1) * 2])
-
+        reg_value = abs(n)
+        mylist = self.generate_serial_list(addr, func_code, reg_addr, num_regs, data_len, reg_value)
         self.ser_send(mylist)
 
     def go_position(self, position):
@@ -104,31 +91,21 @@ class MotorController:
         num_regs = 2
         data_len = 4
         reg_value = position
-
-        data_to_crc = self.int_hex2(addr) + self.int_hex2(func_code) + self.int_hex4(reg_addr) + self.int_hex4(
-            num_regs) + self.int_hex2(data_len) + self.int_hex8(reg_value)
-        # print(data_to_crc)
-        p1, p2 = self.crc16_modbus(data_to_crc)
-
-        data_with_crc = data_to_crc + self.int_hex2(p1) + self.int_hex2(p2)
-        # print(data_with_crc)
-        mylist = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-        for i in range(len(mylist)):
-            mylist[i] = self.str_to_hex('0x' + data_with_crc[i * 2:(i + 1) * 2])
-
+        mylist = self.generate_serial_list(addr, func_code, reg_addr, num_regs, data_len, reg_value)
         self.ser_send(mylist)
 
     def move_angle(self, angle):
         steps_to_take = round(self.full_revolution_steps/360*angle)
-        if angle < 0:
-            self.backward_n_step(abs(steps_to_take))
-        if angle > 0:
-            self.forward_n_step(abs(steps_to_take))
+        self.n_step(abs(steps_to_take))
 
     def reset(self):
         mylist = [0x01, 0x06, 0x07, 0xD0, 0x00, 0x01, 0x48, 0x87]
         self.ser_send(mylist)
-        
+    
+    def move_to_thousand(self):
+        mylist = [0x01, 0x10, 0x07, 0xD2, 0x00, 0x02, 0x04, 0x00, 0x00, 0x03, 0xe8, 0x59, 0xA4]
+        self.ser_send(mylist)
+
     def ser_send(self, data):
         ser = self.ser
         ser.port = self.port
@@ -166,7 +143,6 @@ class MotorController:
 
     def list_com_ports():
         ports = serial.tools.list_ports.comports()
-
         for port, desc, hwid in sorted(ports):
             print("{}: {} [{}]".format(port, desc, hwid))
 
@@ -179,10 +155,12 @@ if __name__ == "__main__":
     mc = MotorController('COM5')
 
     while(True):
-        # mc.go_position(70)
-        mc.move_angle(-133)
+        mc.go_position(7000)
+        # mc.reset()
         # mc.backward_n_step(mc.full_revolution_steps)
         time.sleep(5)
-        mc.move_angle(133)
+        # mc.move_to_thousand()
+        # mc.move_angle(133)
         time.sleep(5)
+        mc.go_position(0)
     
